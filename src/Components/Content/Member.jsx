8e4c3b25b00be3019cc8302fc24554a3efe8ff4b
Rezasfel <rezasfel720@gmail.com>
2024-11-17 13:01:06 +0700

    New Member UI

--- src/Components/Content/Member.jsx
+++ src/Components/Content/Member.jsx
@@ -6,26 +6,32 @@ const Member = () => {
     {
       nama: "Alexandria Loveline Islamsyah",
       link: "https://youtube.com",
+      role: "leader",
     },
     {
       nama: "Anindita Aulia Zahra",
       link: "https://youtube.com",
+      role: "member",
     },
     {
       nama: "Jethro Ibrena Bangun",
       link: "https://youtube.com",
+      role: "member",
     },
     {
       nama: "Revan Adi Pratama",
       link: "https://youtube.com",
+      role: "member",
     },
     {
       nama: "Shahla Nadine Alamudi",
       link: "https://youtube.com",
+      role: "member",
     },
     {
       nama: "Sylvia Febriana Prasetyo",
       link: "https://youtube.com",
+      role: "member",
     },
   ];
 
@@ -33,13 +39,13 @@ const Member = () => {
     <div className="h-screen w-full flex flex-col">
       <div className="w-full h-full flex justify-center items-center flex-col">
         <div className="w-full">
-          <div className="border-2 p-10 h-full w-full  flex flex-col gap-3">
-            {biodata.map(({ nama, link }, index) => {
+          <div className="border-2 p-10 h-full w-full flex justify-center itmes-center gap-3">
+            {biodata.map(({ nama, link, role }, index) => {
               return (
                 <a href={link}>
                   <div
                     key={index}
-                    className="h-8 bg-blue-300 flex justify-center items-center rounded-tr-xl rounded-bl-xl transition duration-500 ease-in-out  hover:bg-blue-700 hover:text-white font-bold"
+                    className="h-48 w-28 flex justify-center items-center rounded-tr-xl rounded-bl-xl transition duration-500 ease-in-out text-center bg-blue-300 hover:bg-blue-700 hover:text-white font-bold"
                   >
                     <h1>{nama}</h1>
                   </div>
@@ -47,12 +53,6 @@ const Member = () => {
               );
             })}
           </div>
-        </div>
-        <div className="border-2 p-10 w-full h-64">
-          <iframe
-            className="h-full w-full rounded-br-2xl rounded-tl-2xl"
-            src="https://youtu.be/POETazMpykQ?si=ygZVVwt4p2XmMH2x"
-          ></iframe>
         </div>
       </div>
     </div>
